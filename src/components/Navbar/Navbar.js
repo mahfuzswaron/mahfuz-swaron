018f2a901ea7116ccf1f8dcdfee3e9b790a030bc
mahfuzswaron <mahfuzswaron@gmail.com>
2024-07-09 20:09:22 +0600

    nav menu updated

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { AiOutlineFilePdf, AiOutlineHome, AiOutlineUser } from 'react-icons/ai';
 import { TbTriangleSquareCircle } from "react-icons/tb";
 import { HiOutlineChatAlt, HiOutlineViewGrid, } from "react-icons/hi";
-import { FaTh } from "react-icons/fa";
+import { VscMenu } from "react-icons/vsc";
 
 
 const Navbar = () => {
@@ -20,36 +20,61 @@ const Navbar = () => {
                 Mahfuz
                 <span className='text-white'> {` }`} </span>
             </h3>
-            <div className="dropdown dropdown-end">
-                <label tabIndex={0} className="cursor-pointer">
-                    <FaTh className='text-2xl text-textBase hover:text-primary' />
-                </label>
-                <ul tabIndex={0} className="dropdown-content grid grid-cols-3 gap-5 p-5 shadow bg-secondary bg-opacity-90 rounded-box w-52">
+            {/* for desktop */}
+            {
+                <ul className='hidden lg:flex gap-5 text-xs font-semibold'>
                     {
                         navLinks.map(l => (
-                            <li key={l.name}>
-                                <a
-                                    title={l.name}
-                                    className='uppercase'
-                                    href={l.path}>
-                                    <l.icon className={
-                                        `
-                                        text-3xl text-textBase hover:text-primary 
-                                        `
-                                    } />  </a>
+                            <li key={l.name} >
+                                <a title={l.name} href={l.path} className='flex gap-x-2 items-center text-textBase hover:text-primary'>
+                                    <l.icon className={``} />
+                                    <span>{l.name.toUpperCase()}</span>
+                                </a>
                             </li>
                         ))
                     }
                     <li>
                         <a
                             title="download resume"
-                            className='uppercase '
                             href={"https://drive.google.com/file/d/1DCzhN3Yik5MpzJoV1sns7XJCTy7_-INr/view?usp=sharing"}
                             target="_blank"
                             rel="noreferrer"
+                            className='flex gap-x-2 items-center text-textBase hover:text-primary'
                         >
+                            <AiOutlineFilePdf className="" />
+                            <span> Resume </span>
+                        </a>
+                    </li>
+                </ul>
+            }
 
-                            <AiOutlineFilePdf className="text-3xl text-textBase hover:text-primary" />
+
+            {/* for mobile */}
+            <div className="dropdown dropdown-end lg:hidden">
+                <label tabIndex={0} className="cursor-pointer">
+                    <VscMenu className='text-2xl text-textBase hover:text-primary' />
+                </label>
+                <ul tabIndex={0} className="dropdown-content grid grid-cols-1 gap-5 p-5 shadow bg-secondary bg-opacity-90 rounded-box w-52">
+                    {
+                        navLinks.map(l => (
+                            <li key={l.name} >
+                                <a title={l.name} href={l.path} className='flex gap-x-2 items-center text-textBase hover:text-primary'>
+                                    <l.icon className={``} />
+                                    <span>{l.name.toUpperCase()}</span>
+                                </a>
+                            </li>
+                        ))
+                    }
+                    <li>
+                        <a
+                            title="download resume"
+                            href={"https://drive.google.com/file/d/1DCzhN3Yik5MpzJoV1sns7XJCTy7_-INr/view?usp=sharing"}
+                            target="_blank"
+                            rel="noreferrer"
+                            className='flex gap-x-2 items-center text-textBase hover:text-primary'
+                        >
+                            <AiOutlineFilePdf className="" />
+                            <span> Resume </span>
                         </a>
                     </li>
                 </ul>
