08d539b06f4071e10812e0793137fb0cda0905d3
mahfuzswaron-sl <swaron@stacklearner.com>
2024-07-14 17:01:04 +0600

    responsiveness of header section fixed

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -9,6 +9,7 @@ const Banner = () => {
     const [text, setText] = useState('');
     const [delta, setDelta] = useState(300 - Math.random() * 100);
     const [index, setIndex] = useState(1);
+    // const toRotate = ["Web Development", "Migrating to Next.js"];
     const toRotate = ["Web Development", "Migrating to Next.js", "Debugging Application", "Email Signature", "Email Template"];
     const period = 500;
     useEffect(() => {
@@ -45,27 +46,28 @@ const Banner = () => {
     }
 
     return (
-        <div id='home' className='grid grid-cols-1 lg:grid-cols-2 justify-between items-center px-5 lg:px-20 pb-20 pt-36 '>
-            <div>
+        <div id='home' className='grid grid-cols-1 md:grid-cols-2  px-5 lg:px-20 pb-20 pt-20 lg:pt-36 '>
+            <div className='flex flex-col gap-y-5 h-[350px] justify-center self-center'>
                 <Headline text={text} />
 
-                <div className='flex space-x-5 mb-5'>
+                <div className='flex space-x-5'>
                     <a
-                        className='btn btn-primary btn-md w-48 my-10'
+                        className='btn btn-primary btn-md w-48'
                         href={"https://calendar.app.google/STztv6tyE4eZ7Txy9"}
                         target="_blank"
                         rel="noreferrer"
                     > Let's discuss </a>
                     <a
-                        className='btn btn-outline btn-md w-36  my-10'
+                        className='btn btn-outline btn-md w-36 '
                         href={"https://drive.google.com/file/d/1DCzhN3Yik5MpzJoV1sns7XJCTy7_-INr/view?usp=sharing"}
                         target="_blank"
                         rel="noreferrer"
                     > resume </a>
                 </div>
             </div>
-            <div className='flex justify-center items-center'>
+            <div className='flex lg:justify-end'>
                 <motion.img
+                    className='w-80'
                     whileHover={{ scale: 1.1 }}
                     transition={{ duration: ANIMATION_DURATION }}
                     src={codingImg}
@@ -79,7 +81,7 @@ export default Banner;
 
 const Headline = ({ text }) => {
     return (
-        <div className='leading-tight h-[250px] sm:h-[180px] lg:h-[220px]'>
+        <div className='leading-tight'>
             <p className='text-[2rem] md:text-[1.8rem] font-light'>Hello, </p>
             <h1 className='text-[2.5rem] md:text-[2rem] font-light'>
                 <span>I'm</span>
